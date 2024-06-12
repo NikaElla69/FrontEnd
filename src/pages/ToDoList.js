8e81026ea4929aaf3b544ebbf88111421ef2a2b1
Camilla Gomes <camilla.gomeseugenio2005@gmail.com>
2024-06-12 09:17:47 -0300

    Commit Aula 03/06-34:24

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./ToDoList.css";
 
 const ToDoList = ({ pageTite }) => {
+    const [toDoList, setToDoList] = useState([])
+    const [inputValue, setInputValue] = useState()
+    const onInputChange = (event) => {
+        console.log(event);
+    }
+
     return (
         <div>
-            <h1>{pageTite}</h1>
-            <div>
-
+            <div className="header">
+                <h1>{pageTite}</h1>
+            </div>
+            <div className="inputContainer">
+                <label for="taskText" className="inputLabel">Digite a sua tarefa: </label>
+                <input
+                    id="taskText"
+                    name="taskText"
+                    className="input"
+                    onChange={onInputChange} />
+                <button className="addButton">+</button>
             </div>
         </div>
     );
