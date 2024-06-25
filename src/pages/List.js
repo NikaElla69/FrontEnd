0fc8d3a3779c8afa5542732f93e0c45fc478ff03
Camilla Gomes <camilla.gomeseugenio2005@gmail.com>
2024-06-25 15:44:50 -0300

    Commit Aula 10/06 ok

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,14 +1,33 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const List = () => {
-    const navigate = useNavigate()
+
+    const [pokemons, setPokemons] = useState([])
+
+    const getPokemons = useCallback(async () => {
+        try {
+            console.log(pokemons);
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon')
+            setPokemons(response.data.results)
+        } catch (error) {
+            console.log(error);
+        }
+    }, [pokemons])
+
+    useEffect(() => {
+        getPokemons();
+    }, [])
+
+    if (!pokemons.length) {
+        return <div>Carregando...</div>
+    }
     return (
         <div>
-            <h3>Músicas:</h3>
-            <p>Zarastruta</p>
-            <p>Yago opróprio</p>
-            <p>Seu Jorge</p>
-            <button onClick={() => navigate(-1)}>Voltar</button>
+            {pokemons.map((pokemon, index) => {
+                return <div key={index}>{pokemon.name}</div>
+            })}
         </div>
     );
 }
